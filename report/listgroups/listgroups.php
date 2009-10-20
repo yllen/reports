51c97001dd949357661af6467e0832662a3f3c31
yllen <yllen@8dedd86a-f2e1-4d8f-975f-694e9cd39626>
2009-10-20 14:39:25 +0000

    listgroups writted for 0.80 with autoreport
    git-svn-id: https://forge.glpi-project.org/svn/reports/trunk@18 8dedd86a-f2e1-4d8f-975f-694e9cd39626

--- report/listgroups/listgroups.php
+++ report/listgroups/listgroups.php
@@ -43,38 +43,31 @@ $DBCONNECTION_REQUIRED=0; // not really a big SQL request
 define('GLPI_ROOT', '../../../..'); 
 include (GLPI_ROOT . "/inc/includes.php"); 
 
-simpleReport(
-	// Report Name
-	"listgroups", 
+$report = new AutoReport();
+//$group = new GroupCriteria($report);
 
-	// SQL statement
-	"SELECT glpi_entities.completename,
-		glpi_groups.name AS groupname,
-		glpi_users.name AS login, 
-		glpi_users.firstname,
-		glpi_users.realname
-	FROM glpi_users_groups
-	LEFT JOIN glpi_users ON (glpi_users_groups.FK_users = glpi_users.ID )
-	LEFT JOIN glpi_groups ON (glpi_users_groups.FK_groups = glpi_groups.ID)
-	LEFT JOIN glpi_entities ON (glpi_groups.FK_entities = glpi_entities.ID)
-	WHERE glpi_users.deleted=0 ".
-	getEntitiesRestrictRequest(" AND ", "glpi_groups") ."
-	ORDER BY glpi_entities.completename, groupname, login",
+$report->setColumnsNames(array('completename' => $LANG["entity"][0],
+                               'groupname'    => $LANG["common"][35],
+                               'login'        => $LANG["setup"][18],
+                               'firstname'    => $LANG["common"][43],
+                               'realname'     => $LANG["common"][48]));
 
+$query = "SELECT `glpi_entities`.`completename`,
+                 `glpi_groups`.`name` AS groupname,
+                 `glpi_users`.`name` AS login, 
+                 `glpi_users`.`firstname`,
+                 `glpi_users`.`realname`
+          FROM `glpi_groups_users`
+          LEFT JOIN `glpi_users` ON (`glpi_groups_users`.`users_id` = `glpi_users`.`id`)
+          LEFT JOIN `glpi_groups` ON (`glpi_groups_users`.`groups_id` = `glpi_groups`.`id`)
+          LEFT JOIN `glpi_entities` ON (`glpi_groups`.`entities_id` = `glpi_entities`.`id`)
+          WHERE `glpi_users`.`is_deleted` = '0' ".
+                getEntitiesRestrictRequest(" AND ", "glpi_groups") ."
+          ORDER BY `completename`, groupname, login";
 
-	// Columns title (optional), from $LANG
-	array (
-		"completename" => $LANG["entity"][0],
-		"groupname" => $LANG["common"][35],
-		"login" => $LANG["setup"][18],
-		"firstname" => $LANG["common"][43],
-		"realname" => $LANG["common"][48]
-		),
-	// Sub title
-	"",
-	
-	// Group by
-	array ("completename","groupname")
-	);
- 
+$report->setGroupBy(array('completename',
+                          'groupname'));
+$report->setSqlRequest($query);
+$report->execute();
+
 ?>
