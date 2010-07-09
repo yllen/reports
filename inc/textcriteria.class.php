ba1c389b1b948f670669a77c43ac70f4fe425848
remi <remi@8dedd86a-f2e1-4d8f-975f-694e9cd39626>
2010-07-09 13:48:21 +0000

    add new criteria : PluginReportsTextCriteria
    git-svn-id: https://forge.glpi-project.org/svn/reports/trunk@88 8dedd86a-f2e1-4d8f-975f-694e9cd39626

---
+++ inc/textcriteria.class.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+  ----------------------------------------------------------------------
+  GLPI - Gestionnaire Libre de Parc Informatique
+  Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+  http://indepnet.net/   http://glpi-project.org/
+  ----------------------------------------------------------------------
+
+  LICENSE
+
+  This file is part of GLPI.
+
+  GLPI is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  GLPI is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with GLPI; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  ------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file:
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+/**
+ * User titles selection criteria
+ */
+class PluginReportsTextCriteria extends PluginReportsDropdownCriteria {
+
+   function __construct($report, $name='value', $label='') {
+      global $LANG;
+
+      parent::__construct($report,$name,'no_table');
+      $this->addCriteriaLabel($name, ($label ? $label :$LANG['common'][16]));
+   }
+
+   function setDefaultValues() {
+      $this->addParameter($this->getName(), '');
+   }
+
+   function displayCriteria() {
+      global $LANG;
+      $this->getReport()->startColumn();
+      echo $this->getCriteriaLabel().'&nbsp;:';
+      $this->getReport()->endColumn();
+
+      $this->getReport()->startColumn();
+      echo "<input type='text' name='".$this->getName()."' value='". $this->getParameterValue() ."'>";
+      $this->getReport()->endColumn();
+   }
+
+   /**
+    * Get criteria's subtitle
+    */
+   public function getSubName() {
+
+      $param = $this->getParameterValue();
+      if ($param) {
+         return $this->getCriteriaLabel().' : '.$this->getParameterValue();
+      }
+      return '';
+   }
+
+   function getSqlCriteriasRestriction($link = 'AND') {
+
+      $param = $this->getParameterValue();
+      if ($param) {
+         return makeTextCriteria($this->getSqlField(), $param, false, $link);
+      }
+      return '';
+   }
+}
+?>
