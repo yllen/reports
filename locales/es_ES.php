a787a6613160543214676dde8e4dd0298504765e
walid <walid@8dedd86a-f2e1-4d8f-975f-694e9cd39626>
2011-05-05 10:03:09 +0000

    Add spanish translation thanks to Jorge López Díaz
    git-svn-id: https://forge.glpi-project.org/svn/reports/trunk@137 8dedd86a-f2e1-4d8f-975f-694e9cd39626

---
+++ locales/es_ES.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: BALPE Dévi
+// Purpose of file:
+// ----------------------------------------------------------------------
+$LANG['plugin_reports']['title'][1] = "Informes";
+
+$LANG['plugin_reports']['config'][1]   = "Configuración del plugin Informes";
+$LANG['plugin_reports']['config'][2]   = "Sin derechos en Utilidades / Informes";
+$LANG['plugin_reports']['config'][3]   = "Sin derechos en Soporte / Estadísticas";
+$LANG['plugin_reports']['config'][4]   = "Derechos por perfil";
+$LANG['plugin_reports']['config'][8]   = "Derechos por informe";
+$LANG['plugin_reports']['config'][9]   = "Derechos de perfiles";
+$LANG['plugin_reports']['config'][10]  = "Informe";
+$LANG['plugin_reports']['config'][11]  = "Configuración del informe";
+
+$LANG['plugin_reports']['reports'][1]  = "Criterios de búsqueda";
+
+$LANG['plugin_reports']['subname']['date-interval']   = "Intervalo fecha";
+$LANG['plugin_reports']['subname']['time-interval']   = "Intervalo hora";
+
+?>
