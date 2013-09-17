7d5be8a9bcb89cbc91585cf2b36858b36dad2f4e
yllen <yllen@8dedd86a-f2e1-4d8f-975f-694e9cd39626>
2013-09-17 11:29:54 +0000

    fix bug for date criteria - fixed #4507
    git-svn-id: https://forge.glpi-project.org/svn/reports/trunk@262 8dedd86a-f2e1-4d8f-975f-694e9cd39626

--- inc/dateintervalcriteria.class.php
+++ inc/dateintervalcriteria.class.php
@@ -58,7 +58,7 @@ class PluginReportsDateIntervalCriteria extends PluginReportsAutoCriteria {
       $start = $this->getParameter($this->getName()."_1");
       $end   = $this->getParameter($this->getName()."_2");
 
-      return (($start == 'NULL') || ($end == 'NULL') || (($start < $end) ? $start : $end));
+      return (($start == 'NULL') || ($end == 'NULL') || ($start < $end) ? $start : $end);
    }
 
 
@@ -67,7 +67,7 @@ class PluginReportsDateIntervalCriteria extends PluginReportsAutoCriteria {
       $start = $this->getParameter($this->getName()."_1");
       $end   = $this->getParameter($this->getName()."_2");
 
-      return (($start == 'NULL') || ($end == 'NULL') || (($start < $end) ? $end : $start));
+      return (($start == 'NULL') || ($end == 'NULL') || ($start < $end) ? $end : $start);
    }
 
 
