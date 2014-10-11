b82f296c690d01072d5f078db4b948d9993555f4
yllen <yllen@8dedd86a-f2e1-4d8f-975f-694e9cd39626>
2014-10-11 17:07:43 +0000

    minor fix - see #4799
    git-svn-id: https://forge.glpi-project.org/svn/reports/trunk@284 8dedd86a-f2e1-4d8f-975f-694e9cd39626

--- inc/usercriteria.class.php
+++ inc/usercriteria.class.php
@@ -38,7 +38,7 @@ class PluginReportsUserCriteria extends PluginReportsDropdownCriteria {
     * @param $name      (default user)
     * @param $label     (default '')
    **/
-   function __construct($report, $name='user_id', $label='') {
+   function __construct($report, $name='users_id', $label='') {
 
       parent::__construct($report, $name, 'glpi_users', ($label ? $label : _n('User', 'Users', 1)));
    }
